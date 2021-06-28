da08791cefa96cd59ea3b8663acae3788657e1fc
Feliphe Paz <feliphe.s.paz@gmail.com>
2021-06-28 18:41:42 -0300

    fetch-bitcoin refatorado

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -1,8 +1,13 @@
-export default function initFetchBitcoin() {
-  fetch('https://blockchain.info/ticker')
-    .then(response => response.json())
-    .then((bitcoin) => {
-      const btcPreco = document.querySelector('.btc-preco');
+export default class FetchBitcoin {
+  constructor(url, targetSelector) {
+    this.url = url;
+    this.targetSelector = targetSelector;
+  }
+
+  init() {
+    fetch(this.url).then(response => response.json()).then((bitcoin) => {
+      const btcPreco = document.querySelector(this.targetSelector);
       btcPreco.innerText = (1000 / bitcoin.BRL.sell).toFixed(4);
     }).catch(erro => console.log(Error(erro)));
+  }
 }
